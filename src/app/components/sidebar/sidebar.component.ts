4e3c653ed4f3194338806ccdf79398508bf188ca
Jeinar03 <jesuseinar2003@gmail.com>
2024-03-16 17:48:58 -0600

    tags agregados

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -16,6 +16,7 @@ export const ROUTES: RouteInfo[] = [
     { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
     { path: '/notifications', title: 'Notificationes',  icon:'notifications', class: '' },
     { path: '/upgrade', title: 'Ajustes',  icon:'unarchive', class: 'active-pro' },
+    { path: '/tags', title: 'tags',  icon: 'dashboard', class: '' },
 ];
 
 @Component({
